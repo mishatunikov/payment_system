fd63b389d94a7131bc53e90c50bf603e5801fc48
mishatunikov <michael.tunikov@gmail.com>
2025-06-01 15:58:42 +0300

    fix: fix regular expression in inn_validator

--- payment/validators.py
+++ payment/validators.py
@@ -3,7 +3,7 @@ from django.core.validators import RegexValidator
 from payment import consts
 
 inn_validator = RegexValidator(
-    regex=rf'^\d{consts.MIN_INN_LENGTH}$|' rf'^\d{consts.MAX_INN_LENGTH}$',
+    regex=rf'^(?:\d{{{consts.MIN_INN_LENGTH}}}|\d{{{consts.MAX_INN_LENGTH}}})$',
     message=f'ИНН должен содержать либо '
     f'{consts.MIN_INN_LENGTH}, либо '
     f'{consts.MAX_INN_LENGTH} цифр',
