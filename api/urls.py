61aa2c6d724039c3777fec3c3fd50fd1e40cca2b
mishatunikov <michael.tunikov@gmail.com>
2025-06-01 20:56:28 +0300

    feat: register organizations/<inn>/balance/ endpoint

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
 
-from api.views import PaymentAPIView
+from api.views import OrganizationBalanceView, PaymentAPIView
 
 urlpatterns = [
     path('webhook/bank/', PaymentAPIView.as_view()),
+    path('organizations/<inn>/balance/', OrganizationBalanceView.as_view()),
 ]
