50c3b707e4f69c802dcd5ec58f8f0a784103ac68
mishatunikov <michael.tunikov@gmail.com>
2025-06-01 21:20:08 +0300

    feat: add config for logging

--- payment_system/settings.py
+++ payment_system/settings.py
@@ -1,12 +1,19 @@
+import logging
 from pathlib import Path
 
 from config import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(filename)s:%(lineno)d #%(levelname)-8s '
+           '[%(asctime)s] - %(name)s - %(message)s',
+)
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config.django.secret_key
 
